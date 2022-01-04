90cdc044b09362abb2a4f1a03f687410090d4f58
g0k1 <89412080+g0k1@users.noreply.github.com>
2022-01-04 16:11:55 -0300

    Create walk code.cs

---
+++ walk code.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    #region Regulars variables
+    [SerializeField] float speed;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float jumpForce;
+    [SerializeField] float cooldownDash;
+    [SerializeField] float dashDuration;
+    bool canJump;
+    bool CanDash;
+    bool faceLeft;
+    float seg;
+    #endregion
+
+
+
+    #region Components
+    Rigidbody2D rb;
+    #endregion
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Jump();
+        detectGround();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 mov = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if(mov != Vector2.zero) 
+        {
+            rb.position += mov * speed * Time.fixedDeltaTime;
+        }
+    }
+    
+    void Jump()
+    {
+        
+        if(canJump)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity += Vector2.up * jumpForce;
+            }
+        }
+    }
+
+    void detectGround()
+    {
+        if (Physics2D.Raycast(transform.position, Vector2.down, .3f, LayerMask.GetMask("Ground")))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+}
