f182537ad799fc09e4c352dacd0af13b7d18015f
g0k1 <89412080+g0k1@users.noreply.github.com>
2022-01-15 22:13:20 -0300

    Create topdown walk code.cs

---
+++ topdown walk code.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class politico : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 mov = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        if (mov != Vector2.zero)
+        {
+            rb.position += mov * speed * Time.fixedDeltaTime;
+        }
+    }
+}
+
