c294b499049e2f1c7b22d31f476c5b648576cbe4
g0k1 <89412080+g0k1@users.noreply.github.com>
2022-01-17 07:53:31 -0300

    Update topdown walk code.cs

--- topdown walk code.cs
+++ topdown walk code.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class politico : MonoBehaviour
+public class player : MonoBehaviour
 {
     [SerializeField] float speed;
 
